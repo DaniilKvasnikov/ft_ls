b8ce2276bba9196c892201eb9345e61fe7754779
daniil.kvasnikov <daniil.kvasnikov@gmail.com>
2019-07-04 19:09:55 +0300

    info

--- includes/ft_ls.h
+++ includes/ft_ls.h
@@ -3,17 +3,23 @@
 # define FT_LS_H
 
 # include "ft_printf.h"
-// #include <stdio.h>
 #include <sys/types.h>
 #include <dirent.h>
+
+#include <sys/types.h> 
+#include <sys/stat.h> 
+#include <unistd.h>
+
+typedef struct  dirent  t_dirent;
+typedef struct  stat    t_stat;
 
 typedef struct	s_path_info
 {
     char        *mode;
-    char        *name;
+    t_dirent    *entry;
+    t_stat      buff;
+    char        *time;
 }				t_path_info;
-
-typedef struct  dirent t_dirent;
 
 char    **set_paths(int argc, char **argv);
 char    *set_flags(int argc, char **argv);
--- src/ft_ls.c
+++ src/ft_ls.c
@@ -1,18 +1,18 @@
 
 #include "ft_ls.h"
 
-#include <sys/types.h> 
-#include <sys/stat.h> 
-#include <unistd.h>
 
 void
 	change_mod(char *mod, int c)
 {
 	int		len;
 
-	mod[0] = (c >> 2 & 1) + '0';
-	mod[1] = (c >> 1 & 1) + '0';
-	mod[2] = (c & 1) + '0';
+	if ((c >> 2 & 1) == 0)
+		mod[0] = '-';
+	if ((c >> 1 & 1) == 0)
+		mod[1] = '-';
+	if ((c & 1) == 0)
+		mod[2] = '-';
 }
 
 char
@@ -26,21 +26,24 @@ char
 		res[0] = '-';
 	len = ft_strlen(mod);
 	change_mod(res + 1, mod[len - 3]);
-	// return (mod);
+	change_mod(res + 4, mod[len - 2]);
+	change_mod(res + 7, mod[len - 1]);
 	return (res);
 }
-
+#include <time.h>
 t_path_info
-	get_info(char *path)
+	get_info(t_dirent *entry)
 {
 	struct stat buff;
 	t_path_info	res;
 	char		*str;
 
-	stat(path, &buff);
+	stat(entry->d_name, &buff);
 	str = ft_rebase(buff.st_mode, 8);
+	res.time = ctime(&buff.st_mtime);
 	res.mode = get_mod(str);
-	res.name = path;
+	res.entry = entry;
+	res.buff = buff;
 	return (res);
 }
 
@@ -78,13 +81,17 @@ void
 	dir = opendir(path);
 	i = 0;
 	while ( (entry = readdir(dir)) != NULL)
-		info[i++] = get_info(entry->d_name);
+		info[i++] = get_info(entry);
 	closedir(dir);
 	
 	i = -1;
 	while (++i < len)
 	{
-		ft_printf("%s %s\n", info[i].mode, info[i].name);
+		ft_printf("%s %10ld %15s %s",
+		info[i].mode,
+		info[i].buff.st_size,
+		info[i].entry->d_name,
+		info[i].time);
 	}
 	return ;
 }
