bb6147a9f6a31dd0767b8d8853012ee958f948fe
daniil.kvasnikov <daniil.kvasnikov@gmail.com>
2019-07-04 09:30:58 +0300

    start mod

--- includes/ft_ls.h
+++ includes/ft_ls.h
@@ -7,6 +7,13 @@
 #include <sys/types.h>
 #include <dirent.h>
 
+typedef struct	s_path_info
+{
+    char        *mode;
+    char        *name;
+}				t_path_info;
+
+typedef struct  dirent t_dirent;
 
 char    **set_paths(int argc, char **argv);
 char    *set_flags(int argc, char **argv);
--- src/ft_ls.c
+++ src/ft_ls.c
@@ -1,32 +1,91 @@
 
 #include "ft_ls.h"
 
+#include <sys/types.h> 
+#include <sys/stat.h> 
+#include <unistd.h>
+
+void
+	change_mod(char *mod, int c)
+{
+	int		len;
+
+	mod[0] = (c >> 2 & 1) + '0';
+	mod[1] = (c >> 1 & 1) + '0';
+	mod[2] = (c & 1) + '0';
+}
+
+char
+	*get_mod(char *mod)
+{
+	char	*res;
+	int		len;
+
+	res = ft_strdup("drwxrwxrwx");
+	if (mod[0] == '1')
+		res[0] = '-';
+	len = ft_strlen(mod);
+	change_mod(res + 1, mod[len - 3]);
+	// return (mod);
+	return (res);
+}
+
+t_path_info
+	get_info(char *path)
+{
+	struct stat buff;
+	t_path_info	res;
+	char		*str;
+
+	stat(path, &buff);
+	str = ft_rebase(buff.st_mode, 8);
+	res.mode = get_mod(str);
+	res.name = path;
+	return (res);
+}
+
+static int
+	ft_count_dir(char *path)
+{
+	DIR			*dir;
+	t_dirent	*entry;
+	int			i;
+	int			len;
+
+	dir = opendir(path);
+	if (!dir)
+		return (-1);
+	i = 0;
+	while ( (entry = readdir(dir)) != NULL)
+		i++;
+	closedir(dir);
+	return (i);
+}
+
 void
     ft_print_dir(char *path)
 {    
-	DIR *dir;
-	struct dirent *entry;
-	int i;
+	DIR			*dir;
+	t_dirent	*entry;
+	int			i;
+	int			len;
+	t_path_info	*info;
 
-	dir = opendir(path);
-	if (!dir) {
-		ft_printf("diropen");
+	len = ft_count_dir(path);
+	if (len < 0)
 		return ;
-	};
-
+	info = (t_path_info *)malloc(sizeof(t_path_info) * len);
+	dir = opendir(path);
 	i = 0;
-	while ( (entry = readdir(dir)) != NULL) {
-		if (i != 0)
-			ft_printf(" ");
-		if (entry->d_name[0] != '.')
-		{
-			ft_printf("%s", entry->d_name);
-			i++;
-		}
-	}
-    ft_putchar('\n');
-
+	while ( (entry = readdir(dir)) != NULL)
+		info[i++] = get_info(entry->d_name);
 	closedir(dir);
+	
+	i = -1;
+	while (++i < len)
+	{
+		ft_printf("%s %s\n", info[i].mode, info[i].name);
+	}
 	return ;
 }
 
